e7bf596c4ab21302bf164854f141156a27aea7c2
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2024-03-20 15:01:41 +0530

    home page services added

--- intellilearn/src/layout/Layout.jsx
+++ intellilearn/src/layout/Layout.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, className }) => {
   return (
-    <div className="layout">
-      <Navbar />
+    <div className={className}>
+      <Navbar className={className} />
       <main>{children}</main>
       <Footer />
     </div>
---
+++ intellilearn/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../layout/Layout";
+const About = () => {
+  return (
+    <Layout>
+      <div className="h-[200vh]">About</div>
+    </Layout>
+  );
+};
+
+export default About;
