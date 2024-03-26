90211217c109c387e3d746de2e285e79cc23f03b
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2024-03-26 16:23:57 +0530

    dummy data

--- intellilearn/src/pages/Dashboard.jsx
+++ intellilearn/src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import DashLay from "../layout/DashLay";
+import Pricing from "../components/Pricing/Pricing";
 
 const Dashboard = () => {
   return (
     <DashLay>
-    dashboard
+      <Pricing />
     </DashLay>
   );
 };
---
+++ intellilearn/src/services/Course.js
@@ -0,0 +1,27 @@
+
+export const dummyData = [
+    {
+        _id: 1,
+        title: 'Web Development Bootcamp',
+        description: 'Learn web development from scratch with this comprehensive bootcamp.',
+        instructor: 'John Doe',
+        price: 99.99,
+        enrollmentLimit: 100,
+        duration: '6 weeks',
+        category: 'Web Development',
+        difficulty: 'beginner',
+        image: 'https://example.com/web_dev_bootcamp.jpg'
+    },
+    {
+        _id: 2,
+        title: 'IT Certifications Crash Course',
+        description: 'Get certified in various IT fields with this crash course.',
+        instructor: 'Jane Smith',
+        price: 129.99,
+        enrollmentLimit: 50,
+        duration: '4 weeks',
+        category: 'IT Certifications',
+        difficulty: 'intermediate',
+        image: 'https://example.com/it_certifications.jpg'
+    },
+];
